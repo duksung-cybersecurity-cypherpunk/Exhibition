e369703eb6fbecb1378f08bcabea9fd700a6122f
Jayde1012 <ariha1982@naver.com>
2024-10-11 16:55:49 +0900

    [FEAT] 메인페이지 useState 적용

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, {useState} from "react";
 import "../styles/Main.css"
 
 const Main = () => {
+    const [text, setText] = useState("메인입니다. Main")
     return (
-        <div className="MainText">메인입니다. Main</div>
+        <div className="MainText">{text}</div>
     )
 }
 
