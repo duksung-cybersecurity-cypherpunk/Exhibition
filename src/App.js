9bb5bbb563e3272434d401b902acd8a944729862
mangodetective <arh052735@gmail.com>
2024-10-13 13:58:02 +0900

    update header and footer
    header, footer 파일 추가

--- src/App.js
+++ src/App.js
@@ -5,10 +5,14 @@ import About from "./pages/About";
 import Project from "./pages/Project";
 import Guestbook from "./pages/Guestbook";
 import Info from "./pages/Info";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 
 function App() {
     return (
         <div className="App">
+            <Header />
             <BrowserRouter basename={process.env.REACT_APP_PUBLIC_URL}>
                 <Routes>
                     <Route path="/" element={<Main />}/>
@@ -18,6 +22,7 @@ function App() {
                     <Route path="/info" element={<Info />}/>
                 </Routes>
             </BrowserRouter>
+            <Footer />
         </div>
     );
 }
