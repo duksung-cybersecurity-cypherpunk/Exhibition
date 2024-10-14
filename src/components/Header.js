3588c8d4f51399c702f13fd5a80eabcce1fd2f07
mangodetective <arh052735@gmail.com>
2024-10-14 20:14:19 +0900

    Footer, Header 수정
    간격조정, 스타일 업데이트
    , 이미지 파일 아직 안됨

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,18 @@
 import React from 'react';
-import './Header.css';  // CSS 파일 불러오기
+import '../styles/Header.css';  // CSS 파일 불러오기
 
 const Header = () => {
   return (
     <header className="header">
         <div className="logo">
-      <div class="header-content">
-        <div class="title">CypherPunk</div>
-        <div class="subtitle">Duksung W.Univ Cyber Security<br></br>2nd Graduation Exhibition</div>
-      </div>
+        <div class="h_title">CypherPunk</div>
+        <div class="h_subtitle">Duksung W.Univ Cyber Security 2nd Graduation Exhibition</div>
       </div>
       <nav className="nav-links">
-        <a href="#about">About</a>
-        <a href="#projects">Projects</a>
-        <a href="#guest-book">Guest Book</a>
-        <a href="#information">Information</a>
+        <a href="#about" className="about">About</a>
+        <a href="#projects" className="projects">Projects</a>
+        <a href="#guest-book" className="guest-book">Guest Book</a>
+        <a href="#information" className="information">Information</a>
       </nav>
     </header>
   );
