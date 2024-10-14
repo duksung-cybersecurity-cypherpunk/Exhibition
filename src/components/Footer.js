eb44c57fc8df6897c11d0656e9510c8b15a489c2
mangodetective <arh052735@gmail.com>
2024-10-14 20:14:19 +0900

    Footer, Header 수정
    간격조정, 스타일 업데이트
    , 이미지 파일 아직 안됨

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import './Footer.css';
+import '../styles/Footer.css';
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer-text">
-        <p>01369| 서울 도봉구 상암로144길 33 (쌍문동, 덕성여자대학교) 덕성여자대학교
+      <div className="footer-group1532">
+        <div className="footer-address">
+        01369|서울 도봉구 삼양로144길 33 (쌍문동, 덕성여자대학교)|덕성여자대학교
         <br></br>02) 901-8677
-        <br></br>Copyright (c) cybersec Language and Literature. All rights reserved.
-        </p>
-    
-      </div>
+        <br></br>Copyright (c) cybersec Language and Literature. All right reserved.
+        </div>
       <div className="footer-icons">
         <a href="" target="_blank" rel="noopener noreferrer">
-          <img src="" alt="Instagram" />
+          <img src="" alt="Insta" />
         </a>
         <a href="" target="_blank" rel="noopener noreferrer">
-          <img src="github-icon.png" alt="GitHub" />
+          <img src="" alt="GitH" />
         </a>
         <a href="" target="_blank" rel="noopener noreferrer">
-          <img src="custom-icon.png" alt="Custom" />
+          <img src="" alt="Cust" />
         </a>
+      </div>
       </div>
     </footer>
   );
