aa12b273a4961b21523e6069a7e552615320b180
Jayde1012 <ariha1982@naver.com>
2024-10-15 22:33:17 +0900

    [FEAT] 메인페이지 생성 (헤더/푸터 제외)

--- src/App.js
+++ src/App.js
@@ -5,10 +5,21 @@ import About from "./pages/About";
 import Project from "./pages/Project";
 import Guestbook from "./pages/Guestbook";
 import Info from "./pages/Info";
+import {isMobile} from "react-device-detect";
+import {useEffect} from "react";
 
 function App() {
+    function setScreenSize() {
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty("--vh", `${vh}px`);
+    }
+
+    useEffect(() => {
+        setScreenSize();
+    });
+
     return (
-        <div className="App">
+        <div className={isMobile? "MobileApp" : "App"}>
             <BrowserRouter basename={process.env.REACT_APP_PUBLIC_URL}>
                 <Routes>
                     <Route path="/" element={<Main />}/>
