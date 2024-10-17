6501cba8ccd1c53d060c7ed429d313915a756b3c
mangodetective <arh052735@gmail.com>
2024-10-17 11:04:42 +0900

    Maybe 95%
    아마 최종최종 예상합니다.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/Header.css';  // CSS 파일 불러오기
 
 const Header = () => {
+  const [selectedLink, setSelelctedLink] = useState('');
+
+  const handleLinkClick = (link) => {
+    setSelelctedLink(link);
+  }
+
   return (
     <header className="header">
       <div className="logo">
-        <div class="hTitle">CypherPunk</div>
-        <div class="hSubtitle">Duksung W.Univ Cyber Security 2nd Graduation Exhibition</div>
+        <div class="headerTitle">CypherPunk</div>
+        <div class="headerSubtitle">Duksung W.Univ Cyber Security 2nd Graduation Exhibition</div>
       </div>
+      
       <nav className="navLinks">
-        <a href="#about" className="about">About</a>
-        <a href="#projects" className="projects">Projects</a>
-        <a href="#guest-book" className="guest-book">Guest Book</a>
-        <a href="#information" className="information">Information</a>
+        <a href="#about"
+          className={`navLink ${selectedLink === 'about' ? 'active' : ''}`}
+          onClick={() => handleLinkClick('about')}>
+          About
+        </a>
+
+        <a href="#about"
+          className={`navLink ${selectedLink === 'about' ? 'active' : ''}`}
+          onClick={() => handleLinkClick('about')}>
+          Projects
+        </a>
+
+        <a href="#guest-book"
+          className={`navLink ${selectedLink === 'guest-book' ? 'active' : ''}`}
+          onClick={() => handleLinkClick('guest-book')}>
+          Guest Book
+        </a>
+
+        <a href="#information"
+          className={`navLink ${selectedLink === 'information' ? 'active' : ''}`}
+          onClick={() => handleLinkClick('information')}>
+          Information
+        </a>
       </nav>
     </header>
   );
