95cfe339f3b0e6052cbc10f8babe681f3b71e139
mangodetective <arh052735@gmail.com>
2024-10-17 12:03:31 +0900

    수정중

--- src/components/AboutButton.js
+++ src/components/AboutButton.js
@@ -7,23 +7,28 @@ const AboutButton = ({ page, setPage }) => {
     const handlePrev = () => {
         if (page > 1) {
             setPage(page - 1);
+        } else if (page === 1) {
+            setPage(5);
         }
     };
 
     const handleNext = () => {
         if (page < 5) {
             setPage(page + 1);
+        } else if (page===5) {
+            setPage(1);
         }
+        
     };
 
     return (
         <div className="pagination">
             <button className="prev" onClick={handlePrev}>
-                <img src={PrevIcon} alt="Previous" />
+                <img src={PrevIcon} alt="Previous" style={{ verticalAlign: 'middle', marginRight: '20px' }}/>
             </button>
-            <span>{page}/5</span>
+            <span style={{ verticalAlign: 'middle', fontSize: '20px' }}>{page}/5</span>
             <button className="next" onClick={handleNext}>
-                <img src={NextIcon} alt="Next" />
+                <img src={NextIcon} alt="Next" style={{ verticalAlign: 'middle', marginLeft: '20px' }}/>
             </button>
         </div>
     );
