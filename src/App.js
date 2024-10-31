127eecde3ceb81f9d5e3701c11c3338d8c55b861
Jayde1012 <ariha1982@naver.com>
2024-10-31 17:42:24 +0900

    [FEAT] about 페이지 모바일 반영, css 수정

--- src/App.js
+++ src/App.js
@@ -54,7 +54,6 @@ function App() {
                 </div>
                 <Footer />
             </BrowserRouter>
-            <About />
         </div>
     );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -28,25 +28,25 @@ const Header = ({headerHandler}) => {
                         <img className="headerMenuImg" src={require("../assets/images/Icon/HeaderMenu.png")} alt="menuBtn"/>
                         </button>
                     : <div className="navLinks">
-                        <Link to="/about" title="About"
+                        <Link to="/Exhibition/about" title="About"
                               className={`navLink ${selectedLink === 'about' ? 'active' : ''}`}
                               onClick={() => handleLinkClick('about')}>
                             About
                         </Link>
 
-                        <Link to="/projects" title="Projects"
+                        <Link to="/Exhibition/projects" title="Projects"
                               className={`navLink ${selectedLink === 'project' ? 'active' : ''}`}
                               onClick={() => handleLinkClick('project')}>
                             Projects
                         </Link>
 
-                        <Link to="/guest" title="Guest Book"
+                        <Link to="/Exhibition/guest" title="Guest Book"
                               className={`navLink ${selectedLink === 'guest-book' ? 'active' : ''}`}
                               onClick={() => handleLinkClick('guest-book')}>
                             Guest Book
                         </Link>
 
-                        <Link to="/info" title="Information"
+                        <Link to="/Exhibition/info" title="Information"
                               className={`navLink ${selectedLink === 'information' ? 'active' : ''}`}
                               onClick={() => handleLinkClick('information')}>
                             Information
